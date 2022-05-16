8b7f41b8e8ec0ce85923abf2d82d9e2d73f3161d
prets <fritz.gerald.dumdum@sun-asterisk.com>
2022-05-16 20:59:28 +0800

    change env key name

--- src/api/base.js
+++ src/api/base.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 const user = Cookies.get('don8_blood');
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_ENDPOINT,
+    baseURL: process.env.REACT_APP_API,
     withCredentials: false,
     headers: {
         Accept: 'application/json',
